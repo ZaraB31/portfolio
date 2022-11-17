e623a0cd04ca612eef7dc13e50b3b7f091e07cd0
Zara Bostock <bostock.zara@gmail.com>
2022-11-17 09:16:25 +0000

    updated package-lock file

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,11 +5,9 @@ function footer() {
     return (
         <footer>
             <article>
-                <a href="" target="_blank"><i class="fa-brands fa-facebook"></i></a>
-                <a href="" target="_blank"><i class="fa-brands fa-twitter"></i></a>
-                <a href="https://www.linkedin.com/in/zara-bostock-49857618a/" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
-                <a href="https://github.com/ZaraB31" target="_blank"><i class="fa-brands fa-github"></i></a>                
-                <a href={ PDF } target="pdf-frame _blank"><i class="fa-solid fa-file-arrow-down"></i></a>
+                <a href="https://www.linkedin.com/in/zara-bostock-49857618a/" target="_blank" rel="noreferrer"><i class="fa-brands fa-linkedin"></i></a>
+                <a href="https://github.com/ZaraB31" target="_blank" rel="noreferrer"><i class="fa-brands fa-github"></i></a>                
+                <a href={ PDF } target="pdf-frame _blank" rel="noreferrer"><i class="fa-solid fa-file-arrow-down"></i></a>
             </article>
             <article>
                 <p>&copy; Zara Bostock 2022</p>
